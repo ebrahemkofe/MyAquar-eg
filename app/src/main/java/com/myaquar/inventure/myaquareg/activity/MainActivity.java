267be08ef6a7c083c2c6488986cc99d9efce3744
Alisamy12 <alisamymohamed@yahoo.com>
2019-01-15 18:26:32 +0200

    iuhlihlih

---
+++ app/src/main/java/com/myaquar/inventure/myaquareg/activity/MainActivity.java
@@ -0,0 +1,33 @@
+package com.myaquar.inventure.myaquareg.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+import com.myaquar.inventure.myaquareg.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<module>mlist=new ArrayList<>();
+    GridView list ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainaa);
+        list =findViewById(R.id.list);
+        mlist.add(new module(R.drawable.user,"des1"));
+        mlist.add(new module(R.drawable.user,"des1"));
+        mlist.add(new module(R.drawable.user,"des1"));
+        mlist.add(new module(R.drawable.user,"des1"));
+        mlist.add(new module(R.drawable.user,"des1"));
+        mlist.add(new module(R.drawable.user,"des1"));
+        mlist.add(new module(R.drawable.user,"des1"));
+
+        Adapter listViewAdapter=new Adapter (getApplicationContext(),R.layout.listitem,  mlist);
+        list.setAdapter(listViewAdapter);
+
+
+    }
+}
---
+++ app/src/main/java/com/myaquar/inventure/myaquareg/activity/module.java
@@ -0,0 +1,18 @@
+package com.myaquar.inventure.myaquareg.activity;
+
+public class module {
+int img ;String name;
+
+    public module(int img, String name) {
+        this.img = img;
+        this.name = name;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
